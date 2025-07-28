d0c0046416fc069a0be97aa05b48ed1352033fdc
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2025-07-28 02:16:30 +0200

    feat: code refactor app

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,38 +1,29 @@
 import type { JSX } from 'react';
-import * as Sentry from '@sentry/react';
-import {
-  useQuery,
-  useMutation,
-  useQueryClient,
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query';
-import { ErrorBoundary } from 'react-error-boundary';
-import { ToastContainer } from 'react-toastify';
-import { BrowserRouter } from 'react-router-dom';
-import { logError } from '@/sentry/logError';
+
 import ErrorFallback from '@/containers/Error/Error';
+import { logError } from '@/sentry/logError';
+import * as Sentry from '@sentry/react';
+import { ErrorBoundary } from 'react-error-boundary';
+import { BrowserRouter } from 'react-router';
+import { ToastContainer } from 'react-toastify';
 import { LanguageProvider } from '@/LanguageProvider';
+import 'react-toastify/dist/ReactToastify.css';
+
 import AuthContext from './AuthContext';
 import Routes from './routes';
-import 'react-toastify/dist/ReactToastify.css';
 import './i18n';
-
-export const queryClient = new QueryClient();
 
 function App(): JSX.Element {
   return (
     <ErrorBoundary FallbackComponent={ErrorFallback} onError={logError}>
-      <QueryClientProvider client={queryClient}>
-        <AuthContext.Provider>
-          <BrowserRouter>
-            <LanguageProvider>
-              <Routes />
-              <ToastContainer />
-            </LanguageProvider>
-          </BrowserRouter>
-        </AuthContext.Provider>
-      </QueryClientProvider>
+      <AuthContext.Provider>
+        <BrowserRouter>
+          <LanguageProvider>
+            <Routes />
+          </LanguageProvider>
+          <ToastContainer />
+        </BrowserRouter>
+      </AuthContext.Provider>
     </ErrorBoundary>
   );
 }
