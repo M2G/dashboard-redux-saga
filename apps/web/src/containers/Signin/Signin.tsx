019159639135402cf64ac4250dfaf896f46adada
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2025-08-09 19:07:04 +0200

    feat: refactor signin container

--- apps/web/src/containers/Signin/Signin.tsx
+++ apps/web/src/containers/Signin/Signin.tsx
@@ -1,24 +1,19 @@
 import { JSX, useCallback } from 'react';
 import { useAuth } from '@/AuthContext';
-import { useDispatch, useSelector } from 'react-redux';
-import { signinUserAction } from '@/store/auth/signin/actions';
 import SigninForm from '@/components/SigninForm';
 import { INITIAL_VALUES } from './constants';
+import { useAuthStore } from '@/store2';
 
 function Signin(): JSX.Element {
-  const dispatch = useDispatch();
+  const login = useAuthStore((state) => state.login);
   const { activateAuth } = useAuth();
 
-  useSelector((state): void => {
-    state?.signin?.data?.accessToken &&
-     activateAuth({ accessToken: state.signin.data.accessToken });
-  });
-
   const onSubmit = useCallback(
-    (e: { email: string; password: string }): void => {
-      dispatch(signinUserAction(e));
+    async (e: { email: string; password: string }): Promise<void> => {
+      const loginUser = await login(e);
+      activateAuth({ accessToken: loginUser?.data?.accessToken });
     },
-    [dispatch],
+    [],
   );
 
   return <SigninForm initialValues={INITIAL_VALUES} onSubmit={onSubmit} />;
