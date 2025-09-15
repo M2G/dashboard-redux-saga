53921584e90a14adcd37916c546332022bee820a
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2025-09-15 02:29:42 +0200

    feat: refactor store auth signin

--- apps/web/src/store/auth/signin/sagas.ts
+++ apps/web/src/store/auth/signin/sagas.ts
@@ -1,31 +1,23 @@
 /* eslint-disable */
 // @see https://github.com/diegohaz/redux-saga-social-login/blob/master/src/store/social/sagas.js
 // @see https://github.com/redux-saga/redux-saga/issues/14
-
 import { all, fork, call, put, take, StrictEffect, TakeEffect, CallEffect } from 'redux-saga/effects';
 import signinService from './services';
 import { SigninActionTypes } from './types';
-import { setAccessTokenStorage } from '@/services/storage';
+import { setAccessTokenStorage } from '@/storage/storage';
 import { signinUserSuccess, signinUserError } from './actions';
 import { signinSuccess } from '@/actions';
 import Config from '@/constants';
-import { history } from '@/index';
-import ROUTER_PATH from '@/constants/RouterPath';
+import ROUTER_PATH from '@/constants/constants';
 
 function forwardTo(history: { push: Function }, location: string) {
-  console.log('history', history);
-    return history.push({ pathname: location });
+  return history.push({ pathname: location });
 }
 
 function* authorize({ ...params }): Generator<StrictEffect, any, any> {
    try {
     const response = yield call(signinService, params);
-
-     console.log('response', response);
-
         const { accessToken } = response?.data;
-
-        console.log('accessToken', accessToken);
 
         Config.GLOBAL_VAR.token = accessToken;
         setAccessTokenStorage(accessToken);
