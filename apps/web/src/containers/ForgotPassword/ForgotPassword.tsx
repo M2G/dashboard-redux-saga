e5c2e94bcffcc9d9e1b86a7be1bfe82b124c3ada
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2025-09-15 02:39:05 +0200

    feat: refactor

--- apps/web/src/containers/ForgotPassword/ForgotPassword.tsx
+++ apps/web/src/containers/ForgotPassword/ForgotPassword.tsx
@@ -1,17 +1,17 @@
 import { useCallback } from 'react';
+import { useDispatch } from 'react-redux';
+import { authForgotPasswordAction } from "@/store/users/actions";
 import { INITIAL_VALUES } from './constants';
 import ForgotPasswordView from './ForgotPassword';
-import { useUserStore } from '@/store2';
 
 function ForgotPassword() {
-  const { forgotPassword } = useUserStore((state) => state);
-  const onSubmit = useCallback((e) => {
-    forgotPassword(e);
-  }, []);
-
-  return (
-    <ForgotPasswordView initialValues={INITIAL_VALUES} onSubmit={onSubmit} />
+  const dispatch = useDispatch();
+  const onSubmit = useCallback(
+    (e) => dispatch(authForgotPasswordAction(e)),
+    [dispatch],
   );
+
+  return <ForgotPasswordView initialValues={INITIAL_VALUES} onSubmit={onSubmit} />;
 }
 
 export default ForgotPassword;
