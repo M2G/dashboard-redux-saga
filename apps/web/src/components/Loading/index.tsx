9f65bfd1e15ad3d699519e9961a2f68161e3800d
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2025-09-15 02:36:49 +0200

    feat: refactor

--- apps/web/src/components/Loading/index.tsx
+++ apps/web/src/components/Loading/index.tsx
@@ -1,21 +1,3 @@
-import * as PropTypes from 'prop-types';
-import * as React from 'react';
 import TopLineLoading from './TopLineLoading';
 
-export interface LoadingNS {
-  isLoading: boolean;
-}
-
-const Loading: React.FC<LoadingNS> = ({ isLoading }: boolean | any) => {
-  if (!isLoading) return null;
-
-  return <TopLineLoading />;
-};
-
-Loading.displayName = 'Loading';
-
-Loading.propTypes = {
-  isLoading: PropTypes.bool.isRequired,
-};
-
-export default Loading;
+export default TopLineLoading;
