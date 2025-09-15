f8f7b1e81e13e9a40123faa58ac35f096471591d
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2025-09-15 02:38:33 +0200

    feat: refactor

--- apps/web/src/index.tsx
+++ apps/web/src/index.tsx
@@ -2,21 +2,31 @@ import SENTRY_CONFIG from '@/sentry/config';
 import { init as initSentry } from '@sentry/react';
 import { createBrowserHistory } from 'history';
 import { createRoot } from 'react-dom/client';
-// import { Provider } from 'react-redux';
+import { Provider } from 'react-redux';
 import App from './App';
-// import configureStore from './configureStore';
+import configureStore from './configureStore';
 import './index.scss';
+import { FC } from 'react';
+import { JSX } from 'react/jsx-runtime';
 
 initSentry(SENTRY_CONFIG);
 
-export const history = createBrowserHistory();
-// export const store = configureStore({} as any);
 
-function render(Component): void {
-  const MOUNT_NODE: any = document.getElementById('root') || document.createElement('div');
+export  const history = createBrowserHistory();
+export const store = configureStore({} as any);
+
+function render(
+  Component: FC<Record<string, any>> | JSX.IntrinsicAttributes,
+): void {
+  const MOUNT_NODE: any =
+    document.getElementById('root') || document.createElement('div');
   const root = createRoot(MOUNT_NODE);
   if (root) {
-    return root.render(<Component history={history} />);
+    return root.render(
+      <Provider store={store}>
+        <Component history={history} />
+      </Provider>,
+    );
   }
 }
 
